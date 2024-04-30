eef89182f6609ad02e0c06b4f1094a4cb8361332
Usman Khan <mgsukhan@gmail.com>
2024-04-30 18:06:01 +0300

    c

--- canuse/stock-analysist/stock_analysis_agents.py
+++ canuse/stock-analysist/stock_analysis_agents.py
@@ -5,62 +5,76 @@ from tools.calculator_tools import CalculatorTools
 from tools.search_tools import SearchTools
 from tools.sec_tools import SECTools
 
+from langchain_groq import ChatGroq
+
+# To Load Local models through groq
+
+llml = ChatGroq(
+    api_key=os.getenv("GROQ_API_KEY"), model="llama3-70b-8192"
+)  # mixtra1-8x7b-32768
+llmm = ChatGroq(
+    api_key=os.getenv("GROQ_API_KEY"), model="mixtra1-8x7b-32768"
+)  # mixtra1-8x7b-32768
 from langchain.tools.yahoo_finance_news import YahooFinanceNewsTool
 
-class StockAnalysisAgents():
-  def financial_analyst(self):
-    return Agent(
-      role='The Best Financial Analyst',
-      goal="""Impress all customers with your financial data 
+
+class StockAnalysisAgents:
+    def financial_analyst(self):
+        return Agent(
+            role="The Best Financial Analyst",
+            goal="""Impress all customers with your financial data 
       and market trends analysis""",
-      backstory="""The most seasoned financial analyst with 
+            backstory="""The most seasoned financial analyst with 
       lots of expertise in stock market analysis and investment
       strategies that is working for a super important customer.""",
-      verbose=True,
-      tools=[
-        BrowserTools.scrape_and_summarize_website,
-        SearchTools.search_internet,
-        CalculatorTools.calculate,
-        SECTools.search_10q,
-        SECTools.search_10k
-      ]
-    )
+            verbose=True,
+            tools=[
+                BrowserTools.scrape_and_summarize_website,
+                SearchTools.search_internet,
+                CalculatorTools.calculate,
+                SECTools.search_10q,
+                SECTools.search_10k,
+            ],
+            llm=llmm,
+        )
 
-  def research_analyst(self):
-    return Agent(
-      role='Staff Research Analyst',
-      goal="""Being the best at gather, interpret data and amaze
+    def research_analyst(self):
+        return Agent(
+            role="Staff Research Analyst",
+            goal="""Being the best at gather, interpret data and amaze
       your customer with it""",
-      backstory="""Known as the BEST research analyst, you're
+            backstory="""Known as the BEST research analyst, you're
       skilled in sifting through news, company announcements, 
       and market sentiments. Now you're working on a super 
       important customer""",
-      verbose=True,
-      tools=[
-        BrowserTools.scrape_and_summarize_website,
-        SearchTools.search_internet,
-        SearchTools.search_news,
-        YahooFinanceNewsTool(),
-        SECTools.search_10q,
-        SECTools.search_10k
-      ]
-  )
+            verbose=True,
+            tools=[
+                BrowserTools.scrape_and_summarize_website,
+                SearchTools.search_internet,
+                SearchTools.search_news,
+                YahooFinanceNewsTool(),
+                SECTools.search_10q,
+                SECTools.search_10k,
+            ],
+            llm=llml,
+        )
 
-  def investment_advisor(self):
-    return Agent(
-      role='Private Investment Advisor',
-      goal="""Impress your customers with full analyses over stocks
+    def investment_advisor(self):
+        return Agent(
+            role="Private Investment Advisor",
+            goal="""Impress your customers with full analyses over stocks
       and completer investment recommendations""",
-      backstory="""You're the most experienced investment advisor
+            backstory="""You're the most experienced investment advisor
       and you combine various analytical insights to formulate
       strategic investment advice. You are now working for
       a super important customer you need to impress.""",
-      verbose=True,
-      tools=[
-        BrowserTools.scrape_and_summarize_website,
-        SearchTools.search_internet,
-        SearchTools.search_news,
-        CalculatorTools.calculate,
-        YahooFinanceNewsTool()
-      ]
-    )
+            verbose=True,
+            tools=[
+                BrowserTools.scrape_and_summarize_website,
+                SearchTools.search_internet,
+                SearchTools.search_news,
+                CalculatorTools.calculate,
+                YahooFinanceNewsTool(),
+            ],
+            llm=llml,
+        )
